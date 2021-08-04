2291cae777804c1b0d51b6fb097f136f6ea5a627
Keith Lucas <keith.lucas@suse.com>
2021-08-04 08:05:17 -0400

    Added function to obtain the pod's namespace.
    Signed-off-by: Keith Lucas <keith.lucas@suse.com>

---
+++ pkg/utils/namespace.go
@@ -0,0 +1,22 @@
+package utils
+
+import (
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+// Get the namespace that the Pod is running in.
+func PodNamespace() string {
+	if ns := os.Getenv("POD_NAMESPACE"); ns != "" {
+		return ns
+	}
+
+	if data, err := ioutil.ReadFile("/var/run/secrets/kubernetes.io/serviceaccount/namespace"); err == nil {
+		if ns := strings.TrimSpace(string(data)); len(ns) > 0 {
+			return ns
+		}
+	}
+
+	return "default"
+}
