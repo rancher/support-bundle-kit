671bfebe1dc4f9650d296ee4a30297f343c0570d
Gaurav Mehta <gaurav@rancher.com>
2023-08-09 14:58:54 +1000

    define a separate filter method for cluster scoped objects

--- pkg/manager/collectors/harvester.go
+++ pkg/manager/collectors/harvester.go
@@ -45,10 +45,10 @@ func (module harvesterModule) generateYAMLs() {
 	}
 
 	dir := filepath.Join(module.c.yamlsDir, "cluster")
-	objs, err := module.c.discovery.ResourcesForCluster(module.toObj, module.skipClusterObjects, module.c.errorLog)
+	objs, err := module.c.discovery.ResourcesForCluster(module.toClusterObj, module.skipClusterObjects, module.c.errorLog)
 
 	if err != nil {
-		logrus.Error("Unable to fetch namespaced resources")
+		logrus.Error("Unable to fetch cluster scoped resources")
 		return
 	}
 
@@ -84,21 +84,6 @@ func (module harvesterModule) toObj(b []byte, groupVersion, kind string, resourc
 			if _, err := jsonParsed.Set(newItems, "items"); err != nil {
 				return nil, err
 			}
-		case "settings":
-			currentItems, _ := jsonParsed.S("items").Data().([]interface{})
-			logrus.Debugf("Whole items: %v", currentItems)
-			var newItems []interface{}
-			for _, item := range currentItems {
-				gItem := gabs.Wrap(item)
-				logrus.Debugf("processing setting %v", gItem.S("metadata", "name").Data().(string))
-				if !slice.ContainsString(ignoreHarvesterSettingsList, gItem.S("metadata", "name").Data().(string)) {
-					logrus.Debugf("Prepare to append item: %v", gItem.Data().(map[string]interface{}))
-					newItems = append(newItems, item)
-				}
-			}
-			if _, err := jsonParsed.Set(newItems, "items"); err != nil {
-				return nil, err
-			}
 		default:
 			// undefined resource, just logged it.
 			logrus.Warnf("Could not handle unknown resource %s", resource)
@@ -118,9 +103,40 @@ func getHarvesterExtraResource() map[string][]string {
 // skipClusterObjects implements the ExcludeFilter and skips all cluster resources
 // other than settings.harvesterhci.io
 func (module harvesterModule) skipClusterObjects(gv schema.GroupVersion, resource v1.APIResource) bool {
-	if gv.Group == "harvesterhci.io" && gv.Version == "v1beta1" && resource.Name == "settings" {
+	if gv.Group == "harvesterhci.io" && gv.Version == "v1beta1" && resource.Kind == "Setting" {
+		logrus.Debugf("processing object %s with gv %s\n", resource.String(), gv.String())
 		return false
 	}
 	logrus.Debugf("skipping object %s with gv %s\n", resource.String(), gv.String())
 	return true
+}
+
+func (module harvesterModule) toClusterObj(b []byte, groupVersion, kind string, resources ...string) (interface{}, error) {
+	jsonParsed, err := module.c.toObjCommon(b, groupVersion, kind)
+
+	if err != nil {
+		return nil, err
+	}
+
+	switch kind {
+	case "Setting":
+		currentItems, _ := jsonParsed.S("items").Data().([]interface{})
+		logrus.Debugf("Whole items: %v", currentItems)
+		var newItems []interface{}
+		for _, item := range currentItems {
+			gItem := gabs.Wrap(item)
+			logrus.Debugf("processing setting %v", gItem.S("metadata", "name").Data().(string))
+			if !slice.ContainsString(ignoreHarvesterSettingsList, gItem.S("metadata", "name").Data().(string)) {
+				logrus.Debugf("Prepare to append item: %v", gItem.Data().(map[string]interface{}))
+				newItems = append(newItems, item)
+			}
+		}
+		if _, err := jsonParsed.Set(newItems, "items"); err != nil {
+			return nil, err
+		}
+	default:
+		// undefined resource, just logged it.
+		logrus.Warnf("Could not handle kind %s", kind)
+	}
+	return jsonParsed.Data(), nil
 }
