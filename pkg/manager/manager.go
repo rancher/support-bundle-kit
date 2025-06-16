89473bf8f38ae02c7d14cf0f3cbda1992a442b60
Chin-Ya Huang <chin-ya.huang@suse.com>
2025-06-16 09:18:01 +0800

    feat(ipv6): add address family support when collecting node bundle
    longhorn/longhorn-2259
    
    Signed-off-by: Chin-Ya Huang <chin-ya.huang@suse.com>

--- pkg/manager/manager.go
+++ pkg/manager/manager.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"net"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -361,7 +362,7 @@ func (m *SupportBundleManager) collectNodeBundles() error {
 
 	// create a daemonset to collect node bundles and push back
 	agents := &AgentDaemonSet{sbm: m}
-	agentDaemonSet, err := agents.Create(m.ImageName, fmt.Sprintf("http://%s:8080", m.ManagerPodIP))
+	agentDaemonSet, err := agents.Create(m.ImageName, fmt.Sprintf("http://%s", net.JoinHostPort(m.ManagerPodIP, "8080")))
 	if err != nil {
 		return err
 	}
